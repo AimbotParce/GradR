ae564468cc21bbbf16008daa7f6c21f37867a1da
AimbotParce <marcparcerisa@gmail.com>
2026-01-27 14:01:20 +0100

    Make database configurable

--- gradr/__main__.py
+++ gradr/__main__.py
@@ -1,14 +1,23 @@
 import asyncio
+from pathlib import Path
 
-from .database import create_tables
+from .database import create_tables, register_database
 from .ui import GradingApp
 
 
-async def main():
+async def main(db_path: Path):
+    await register_database(db_path)
     await create_tables()
     app = GradingApp()
     await app.run_async()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Gradr - Manage grading projects and tasks from the terminal.")
+    parser.add_argument("database", nargs="?", default="grading.db", help="Path to the SQLite database file.")
+    args = parser.parse_args()
+
+    db_path = Path(args.database)
+    asyncio.run(main(db_path))
--- gradr/database/__init__.py
+++ gradr/database/__init__.py
@@ -1,31 +1,42 @@
 import os
 from contextlib import asynccontextmanager
+from pathlib import Path
 from typing import AsyncIterator
 
 from sqlalchemy.engine import URL
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 
-def get_async_engine():
-    async_database_url = URL.create(
-        drivername="sqlite+aiosqlite",
-        database="projectgrading.db",
-    )
+def get_async_engine(db_path: str | Path = "grading.db"):
+    async_database_url = URL.create(drivername="sqlite+aiosqlite", database=str(db_path))
     async_engine = create_async_engine(async_database_url)
     return async_engine
 
 
-async_engine = get_async_engine()
-AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+async_engine: AsyncEngine
+AsyncSessionLocal: async_sessionmaker
 
 
 @asynccontextmanager
 async def get_async_session() -> AsyncIterator[AsyncSession]:
+    assert "AsyncSessionLocal" in globals(), "Database not registered. Call register_database first."
     async with AsyncSessionLocal() as session:
         yield session
 
 
+async def register_database(db_path: str | Path = "grading.db"):
+    global async_engine, AsyncSessionLocal
+    async_engine = get_async_engine(db_path)
+    AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+
+
 async def create_tables():
+    assert "async_engine" in globals(), "Database not registered. Call register_database first."
     from .models import Base  # Import models to register them with Base
 
     async with async_engine.begin() as conn:
